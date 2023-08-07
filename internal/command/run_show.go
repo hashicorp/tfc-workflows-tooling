41bc3d83897952ca9e4acf2a931bd1ca1761a5de
Michael Yocca <michael.yocca@hashicorp.com>
2023-08-07 09:54:59 -0700

    Fix missing default for platformOut option

--- internal/command/run_show.go
+++ internal/command/run_show.go
@@ -86,8 +86,9 @@ func (c *ShowRunCommand) addRunDetails(run *tfe.Run) {
 	}
 
 	c.addOutputWithOpts("payload", run, &outputOpts{
-		stdOut:    false,
-		multiLine: true,
+		stdOut:      false,
+		multiLine:   true,
+		platformOut: true,
 	})
 }
 
